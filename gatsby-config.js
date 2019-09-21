629f9fa5e1d582de2cdbe6b511cb19fdd1120e94
Maylie <35114391+Maylie1918@users.noreply.github.com>
2019-09-21 10:24:27 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/reabnormal/8e01abd03643497abb2f98ac2512bb29?v=59b694f761f14af1be865f997e001611"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/reabnormal/cbe8a5a281c045b2a5937438466e801f?v=fa9c613f8f8e457195f0288de82d8ccb"
             }
         }
     ],
